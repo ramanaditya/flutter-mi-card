7a44e5a6a9af72380d3b577cccbc3fd59a4dfb3e
ramanaditya <adityaraman96@gmail.com>
2020-01-20 18:35:25 +0530

    Mi Card in Flutter

---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('assets/images/adi_pic.jpg'),
+                ),
+                Text(
+                  "Aditya Raman",
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Pacifico",
+                  ),
+                ),
+                Text(
+                  "FLUTTER DEVELOPER",
+                  style: TextStyle(
+                    fontFamily: "Source Sans Pro",
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+//                Card(
+//                  color: Colors.white,
+//                  margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+//                  child: Padding(
+//                    padding: EdgeInsets.all(25.0),
+//                    child: Row(
+//                      children: <Widget>[
+//                        Icon(
+//                          Icons.phone,
+//                          color: Colors.teal,
+//                        ),
+//                        SizedBox(
+//                          width: 10.0,
+//                        ),
+//                        Text(
+//                          "+91-99015-61092",
+//                          style: TextStyle(
+//                            color: Colors.teal.shade900,
+//                            fontFamily: "Source Sans Pro",
+//                            fontSize: 20.0,
+//
+//                          ),
+//                        )
+//                      ],
+//                    ),
+//                  ),
+//                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  )
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "+91-99015-61092",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: "Source Sans Pro",
+                        fontSize: 20.0,
+
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      "adityaraman96@gmail.com",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: "Source Sans Pro",
+                        fontSize: 20.0,
+
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
